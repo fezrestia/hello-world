cf711ffd6ef5e1884277cd74623cd7aee4939f2f
Aki.SHIMO <fezrestia@gmail.com>
2015-07-22 19:27:56 +0900

    ADD:Include Velocity template rendering engine

--- src/com/fezrestia/gae/memo/MainPage.java
+++ src/com/fezrestia/gae/memo/MainPage.java
@@ -1,75 +1,42 @@
 package com.fezrestia.gae.memo;
 
 import java.io.IOException;
-import java.text.MessageFormat;
 import java.util.List;
 import java.util.TimeZone;
 
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.context.Context;
 
 import com.fezrestia.gae.util.PMF;
+import com.fezrestia.gae.velocity.Renderer;
 
 @SuppressWarnings("serial")
 public class MainPage extends HttpServlet {
-
-    private static final String HEAD =
-            "<html>\n" +
-            "<body>\n" +
-            "  <span style=\"font-size: 200%\">MEMO</span>\n" +
-            "    <form action=\"/memoNew\" method=\"post\">\n" +
-            "      <div>\n" +
-            "        <textarea name=\"content\" rows=\"2\" cols=\"40\"></textarea>" +
-            "      </div>\n" +
-            "      <input type=\"submit\" value=\"SUBMIT\" />\n" +
-            "    </form>\n";
-
-    private static final String MEMO_TEMPLATE =
-            "    <div>\n" +
-            "      <span class=\"date\"> {0, time} {0, date} </span>\n" +
-            "      <pre>{1}</pre>\n" +
-            "    </div>\n";
-
-    private static final String TAIL =
-            "</body>\n" +
-            "</html>\n";
-
     static {
         TimeZone.setDefault(TimeZone.getTimeZone("JST"));
     }
 
-    private String render(List<Memo> memoList) {
-        StringBuilder builder = new StringBuilder().append(HEAD);
-        for (Memo eachMemo : memoList) {
-            builder.append(MessageFormat.format(
-                    MEMO_TEMPLATE,
-                    eachMemo.getDate(),
-                    eachMemo.getContent()));
-        }
-        builder.append(TAIL);
-
-        return builder.toString();
-    }
-
-
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        PersistenceManager pm = null;
-        try {
-            pm = PMF.get().getPersistenceManager();
-            Query query = pm.newQuery(Memo.class);
-            query.setOrdering("date desc");
+        PersistenceManager pm = PMF.get().getPersistenceManager();
 
-            List<Memo> memos = (List<Memo>) query.execute();
+        Query query = pm.newQuery(Memo.class);
+        query.setOrdering("date desc");
 
-            resp.setContentType("text/html");
-            resp.setCharacterEncoding("utf-8");
-            resp.getWriter().print(render(memos));
-        } finally {
-            if (pm != null && !pm.isClosed()) {
-                pm.close();
-            }
-        }
+        List<Memo> memos = (List<Memo>) query.execute();
+
+        Context context = new VelocityContext();
+        context.put("memos",  memos);
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+
+        Renderer.render("WEB-INF/memoMainPage.vm", context, resp.getWriter());
     }
 }
