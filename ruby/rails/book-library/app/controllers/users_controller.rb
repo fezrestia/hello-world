bfb3e2a15c291c0d4bcd51159dee8afa1915ec81
Aki.SHIMO <fezrestia@gmail.com>
2016-04-16 12:02:56 +0900

    ADD:ruby/ Book and user management

---
+++ ruby/rails/book-library/app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+    # Filter
+    before_action :set_user, only: %w(show edit update destroy)
+
+    def index
+        @users = User.all
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to users_url
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        # NOP
+    end
+
+    def edit
+        # NOP
+    end
+
+    def update
+        if @user.update(user_params)
+            redirect_to user_url(@user)
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @user.destroy
+        redirect_to users_url
+    end
+
+private
+    def user_params
+        params.require(:user).permit(:name, :department)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+end
+
--- ruby/rails/book-library/config/routes.rb
+++ ruby/rails/book-library/config/routes.rb
@@ -1,4 +1,14 @@
-Rails.application.routes.draw do
+
+BookLibrary::Application.routes.draw do
+    resources :books
+#    resources :users, only: %i(index new create)
+    resources :users
+end
+
+
+
+#Rails.application.routes.draw do
+#  resources :books
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +63,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+#end
+
