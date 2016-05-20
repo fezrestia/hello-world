fb8ed0fb05f112ab53e38d96fb107f92796ebdc3
Aki.SHIMO <fezrestia@gmail.com>
2016-05-21 00:54:51 +0900

    ADD:rails-tutorials/ Users list and admin functions

--- ruby/rails/rails-tutorials/sample-app/test/integration/site_layout_test.rb
+++ ruby/rails/rails-tutorials/sample-app/test/integration/site_layout_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:rachael)
+  end
+
+  test "layout links with no user logged in" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -15,6 +19,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_response :success
     assert_select "title", full_page_title("Sign up")
+  end
+
+  test "layout links with user logged in" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
   end
 
 end
--- ruby/rails/rails-tutorials/sample-app/test/integration/users_edit_test.rb
+++ ruby/rails/rails-tutorials/sample-app/test/integration/users_edit_test.rb
@@ -20,8 +20,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
+    assert_equal edit_user_url(@user), session[:forwarding_url]
     log_in_as(@user)
     assert_redirected_to edit_user_path(@user)
+    assert_nil session[:forwarding_url]
     name = "Hoge Fuga Piyo"
     email = "hoge.fuga@piyo.com"
     patch user_path(@user), user: {
