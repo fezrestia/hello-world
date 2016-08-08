cb3c169f00870a938f481ff0aaca19c04e651b02
Aki.SHIMO <fezrestia@gmail.com>
2016-08-08 19:13:51 +0900

    ADD: rails-tutorials/ Password reset function

--- ruby/rails/rails-tutorials/sample-app/app/models/user.rb
+++ ruby/rails/rails-tutorials/sample-app/app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 
   attr_accessor :remember_token
   attr_accessor :activation_token
+  attr_accessor :reset_token
 
 
   # Validation
@@ -76,6 +77,26 @@ class User < ActiveRecord::Base
   #
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Create password reset digest.
+  #
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Send password reset mail.
+  #
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Check password reset is valid or not.
+  #
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
