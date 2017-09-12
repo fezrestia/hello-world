dec6983b4ce2391469941fbe9e59f3eed573f60c
Aki.SHIMO <fezrestia@gmail.com>
2017-09-12 20:22:39 +0900

    ADD: eager_load sample

--- ruby/rails/sandbox/app/controllers/sample_controller.rb
+++ ruby/rails/sandbox/app/controllers/sample_controller.rb
@@ -175,6 +175,35 @@ logger.debug("attrs = #{attrs}")
       ret << "    " << attrs.to_s << "\n"
     }
 
+    ret << "\n"
+
+    log = '## Color->Model->Platform ALL SELECT with ActiveRecord'
+    ret << log << "\n"
+    logger.debug(log)
+    Color.eager_load( :models => :platform )
+        .select("colors.*, models.*, platforms.*, colors.id AS color_id, colors.name AS color_name, models.id AS model_id, models.name AS model_name, platforms.id AS platform_id, platforms.name AS platform_name")
+        .where("model_name = ? OR model_name IS NULL", "Rachael")
+        .each { |record|
+
+logger.debug("########")
+#logger.debug("record = #{record.to_s}")
+#logger.debug("Color.instance_methods(true) = #{Color.instance_methods(true)}")
+#logger.debug("record.variables = #{record.instance_variables}")
+#logger.debug("record.models.variables = #{record.models.instance_variables}")
+#logger.debug("record.models.methods = #{Model.instance_methods(true)}")
+logger.debug"## record.models[0] = #{record.models[0]}, len=#{record.models.size}"
+
+          if record.models[0].nil?
+              ret << "    color_id=#{record.id}, color_name=#{record.name}, model_id=null, model_name=null, platform_id=null, platform_name=null\n"
+          else
+            record.models.each { |model|
+              ret << "    color_id=#{record.id}, color_name=#{record.name}, model_id=#{model.id}, model_name=#{model.name}, platform_id=#{model.platform.id}, platform_name=#{model.platform.name}\n"
+            }
+          end
+        }
+
+    ret << "\n"
+
 
 
     render plain: ret
