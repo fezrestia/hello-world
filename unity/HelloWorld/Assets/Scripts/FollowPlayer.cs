7d7d38d1581d0896068f02b378fa7cd37ff8dd39
Aki.SHIMO <fezrestia@gmail.com>
2019-01-04 18:37:03 +0900

    ADD: Add collider samples to Unity HelloWorld

---
+++ unity/HelloWorld/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform target = null;
+
+    private Vector3 offset; 
+
+    void Start()
+    {
+        offset = GetComponent<Transform>().position - target.position;
+
+    }
+
+    void Update()
+    {
+        GetComponent<Transform>().position = target.position + offset;
+
+    }
+
+
+}
