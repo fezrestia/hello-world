25822b50f61efafa3eaaf5b08e337d8d7f389d05
Aki.SHIMO <fezrestia@gmail.com>
2026-07-07 08:45:44 +0000

    ADD: deep learning from zero 5 : 6 newral network

--- deep-learning/generative-diffusion/main.py
+++ deep-learning/generative-diffusion/main.py
@@ -6,10 +6,16 @@ from pathlib import Path
 import os
 from scipy.stats import norm
 from typing import Any
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision  # type: ignore[import-untyped]
+import torchvision.transforms as transforms  # type: ignore[import-untyped]
 
 SCRIPT_DIR = Path(__file__).resolve().parent
 
 np.random.seed(0)
+torch.manual_seed(0)
 
 
 
@@ -385,19 +391,19 @@ def gmm(
 
 # Estimation-Maximization Algorithm
 
-path = os.path.join(SCRIPT_DIR, "dataset/old_faithful.txt")
-xs = np.loadtxt(path)
-print(f"old_faithful : xs.shape = {xs.shape}")
-
-phis = np.array([0.5, 0.5])
-mus = np.array([[0.0, 50.0],
-                [0.0, 100.0]])
-covs = np.array([np.eye(2), np.eye(2)])  # identity matrix
-
-K = len(phis)  # 2
-N = len(xs)  # 272
-MAX_ITERS = 100
-THRESHOLD = 1e-4
+#path = os.path.join(SCRIPT_DIR, "dataset/old_faithful.txt")
+#xs = np.loadtxt(path)
+#print(f"old_faithful : xs.shape = {xs.shape}")
+#
+#phis = np.array([0.5, 0.5])
+#mus = np.array([[0.0, 50.0],
+#                [0.0, 100.0]])
+#covs = np.array([np.eye(2), np.eye(2)])  # identity matrix
+#
+#K = len(phis)  # 2
+#N = len(xs)  # 272
+#MAX_ITERS = 100
+#THRESHOLD = 1e-4
 
 
 
@@ -424,82 +430,306 @@ def likelihood(
 
 
 
-current_likelihood = likelihood(xs, phis, mus, covs)
+#current_likelihood = likelihood(xs, phis, mus, covs)
+#
+#for iter in range(MAX_ITERS):
+#    # E step
+#    qs = np.zeros((N, K))  # N: data dimens, K: phi len
+#    for n in range(N):
+#        x = xs[n]
+#        for k in range(K):
+#            phi = phis[k]
+#            mu = mus[k]
+#            cov = covs[k]
+#
+#            qs[n, k] = phi * multi_dim_normal(x, mu, cov)
+#        qs[n] /= gmm(x, phis, mus, covs)
+#
+#    # M step
+#    qs_sum = qs.sum(axis = 0)
+#    for k in range(K):
+#        # phi
+#        phis[k] = qs_sum[k] / N
+#
+#        # mu
+#        c = 0
+#        for n in range(N):
+#            c += qs[n, k] * xs[n]
+#        mus[k] = c / qs_sum[k]
+#
+#        # cov
+#        c = 0
+#        for n in range(N):
+#            z = xs[n] - mus[k]
+#            z = z[:, np.newaxis]
+#            c += qs[n, k] * z @ z.T
+#        covs[k] = c / qs_sum[k]
+#
+#    # converged or not
+#    print(f"current_likelihood = {current_likelihood:.3f}")
+#
+#    next_likelihood = likelihood(xs, phis, mus, covs)
+#
+#    diff = np.abs(next_likelihood - current_likelihood)
+#    if diff < THRESHOLD:
+#        break
+#
+#    current_likelihood = next_likelihood
+#
+#
+#N = 500
+#new_xs = np.zeros((N, 2))
+#for n in range(N):
+#    k = np.random.choice(2, p = phis)
+#    mu = mus[k]
+#    cov = covs[k]
+#    new_xs[n] = np.random.multivariate_normal(mu, cov)
+#
+#
+## visualize
+#def plot_contour(w, mus, covs):
+#    x = np.arange(1, 6, 0.1)
+#    y = np.arange(40, 100, 1)
+#    X, Y = np.meshgrid(x, y)
+#    Z = np.zeros_like(X)
+#
+#    for i in range(X.shape[0]):
+#        for j in range(X.shape[1]):
+#            x = np.array([X[i, j], Y[i, j]])
+#
+#            for k in range(len(mus)):
+#                mu, cov = mus[k], covs[k]
+#                Z[i, j] += w[k] * multi_dim_normal(x, mu, cov)
+#    plt.contour(X, Y, Z)
+#
+#plt.scatter(xs[:,0], xs[:,1], alpha = 0.7, label ="original")
+#plt.scatter(new_xs[:,0], new_xs[:,1], alpha = 0.7, label = "generated")
+#plot_contour(phis, mus, covs)
+#plt.xlabel("eruptions[min])")
+#plt.ylabel("waiting[min]")
+#plt.show()
 
-for iter in range(MAX_ITERS):
-    # E step
-    qs = np.zeros((N, K))  # N: data dimens, K: phi len
-    for n in range(N):
-        x = xs[n]
-        for k in range(K):
-            phi = phis[k]
-            mu = mus[k]
-            cov = covs[k]
-
-            qs[n, k] = phi * multi_dim_normal(x, mu, cov)
-        qs[n] /= gmm(x, phis, mus, covs)
-
-    # M step
-    qs_sum = qs.sum(axis = 0)
-    for k in range(K):
-        # phi
-        phis[k] = qs_sum[k] / N
-
-        # mu
-        c = 0
-        for n in range(N):
-            c += qs[n, k] * xs[n]
-        mus[k] = c / qs_sum[k]
-
-        # cov
-        c = 0
-        for n in range(N):
-            z = xs[n] - mus[k]
-            z = z[:, np.newaxis]
-            c += qs[n, k] * z @ z.T
-        covs[k] = c / qs_sum[k]
-
-    # converged or not
-    print(f"current_likelihood = {current_likelihood:.3f}")
-
-    next_likelihood = likelihood(xs, phis, mus, covs)
-
-    diff = np.abs(next_likelihood - current_likelihood)
-    if diff < THRESHOLD:
-        break
-
-    current_likelihood = next_likelihood
 
 
-N = 500
-new_xs = np.zeros((N, 2))
-for n in range(N):
-    k = np.random.choice(2, p = phis)
-    mu = mus[k]
-    cov = covs[k]
-    new_xs[n] = np.random.multivariate_normal(mu, cov)
+# torch
+
+#def rosenbrock(x0, x1):
+#    y = 100 * (x1 - x0 ** 2) ** 2 + (x0 - 1) ** 2
+#    return y
+#
+#x0 = torch.tensor(0.0, requires_grad = True)
+#x1 = torch.tensor(2.0, requires_grad = True)
+#
+#lr = 0.001
+#iters = 10000
+#
+#for i in range(iters):
+#    if i % 1000 == 0:
+#        print(x0.item(), x1.item())
+#
+#    y = rosenbrock(x0, x1)
+#
+#    y.backward()
+#
+#    if x0.grad is not None and x1.grad is not None:
+#        x0.data -= lr * x0.grad.data
+#        x1.data -= lr * x1.grad.data
+#
+#        x0.grad.zero_()
+#        x1.grad.zero_()
+#    else:
+#        raise Exception("x0.grad or x1.grad is None")
+#
+#print(x0.item(), x1.item())
+#
+#
+#x = torch.rand(100, 1)
+#y = 2 * x + 5 + torch.rand(100, 1)
+#
+#W = torch.zeros((1, 1), requires_grad = True)
+#b = torch.zeros(1, requires_grad = True)
+#
+#def predict(x):
+#    y = x @ W + b
+#    return y
 
 
-# visualize
-def plot_contour(w, mus, covs):
-    x = np.arange(1, 6, 0.1)
-    y = np.arange(40, 100, 1)
-    X, Y = np.meshgrid(x, y)
-    Z = np.zeros_like(X)
 
-    for i in range(X.shape[0]):
-        for j in range(X.shape[1]):
-            x = np.array([X[i, j], Y[i, j]])
+def mean_squared_error(x0, x1):
+    diff = x0 - x1
+    N = len(diff)
+    return torch.sum(diff ** 2) / N
 
-            for k in range(len(mus)):
-                mu, cov = mus[k], covs[k]
-                Z[i, j] += w[k] * multi_dim_normal(x, mu, cov)
-    plt.contour(X, Y, Z)
 
-plt.scatter(xs[:,0], xs[:,1], alpha = 0.7, label ="original")
-plt.scatter(new_xs[:,0], new_xs[:,1], alpha = 0.7, label = "generated")
-plot_contour(phis, mus, covs)
-plt.xlabel("eruptions[min])")
-plt.ylabel("waiting[min]")
+
+#lr = 0.1
+#iters = 100
+#for i in range(iters):
+#    y_hat = predict(x)
+#    loss = mean_squared_error(y, y_hat)
+#
+#    loss.backward()
+#
+#    if W.grad is None or b.grad is None:
+#        raise Exception("W or b is None.")
+#
+#    W.data -= lr * W.grad.data
+#    b.data -= lr * b.grad.data
+#
+#    W.grad.zero_()
+#    b.grad.zero_()
+#
+#    if i % 10 == 0:
+#        print(f"loss = {loss.item()}")
+#
+#print(f"loss = {loss.item()}")
+#print(f"W = {W}")
+#print(f"b = {b}")
+
+
+#W = nn.Parameter(torch.zeros(1, 1))
+#b = nn.Parameter(torch.zeros(1))
+#
+#print(W)
+#print(b)
+
+
+#class Model(nn.Module):
+#    def __init__(self):
+#        super().__init__()
+#
+#        self.W = nn.Parameter(torch.zeros(1, 1))
+#        self.b = nn.Parameter(torch.zeros(1))
+#
+#    def forward(self, x):
+#        y = x @ self.W + self.b
+#        return y
+#
+#
+#model = Model()
+#
+#for param in model.parameters():
+#    print(param)
+
+
+#class Model(nn.Module):
+#    def __init__(self, input_size = 1, output_size = 1):
+#        super().__init__()
+#
+#        self.linear = nn.Linear(input_size, output_size)
+#
+#    def forward(self, x):
+#        y = self.linear(x)
+#        return y
+#
+#model = Model()
+#for param in model.parameters():
+#    print(param)
+#
+#
+#x = torch.rand(100, 1)
+#y = 2 * x + 5 + torch.rand(100, 1)
+#lr = 0.1
+#iters = 100
+#
+#
+##optimizer = torch.optim.SGD(model.parameters(), lr = lr)
+#optimizer = torch.optim.Adam(model.parameters(), lr = lr)
+#
+#for i in range(iters):
+#    y_hat = model(x)
+#
+#    loss = nn.functional.mse_loss(y, y_hat)
+#
+#    loss.backward()
+#
+#    optimizer.step()
+#    optimizer.zero_grad()
+
+
+x = torch.rand(100, 1)
+y = torch.sin(2 * torch.pi * x) + torch.rand(100, 1)
+
+class Model(nn.Module):
+    def __init__(self, input_size = 1, hidden_size = 10, output_size = 1):
+        super().__init__()
+
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        y = self.linear1(x)
+        y = F.sigmoid(y)
+        y = self.linear2(y)
+        return y
+
+lr = 0.2
+iters = 10000
+
+model = Model()
+optimizer = torch.optim.SGD(model.parameters(), lr = lr)
+
+for i in range(iters):
+    y_pred = model(x)
+    loss = F.mse_loss(y, y_pred)
+
+    loss.backward()
+
+    optimizer.step()
+    optimizer.zero_grad()
+
+    if i % 1000 == 0:
+        print(loss.item())
+
+print(loss.item())
+
+# plot
+plt.scatter(x.detach().numpy(), y.detach().numpy(), s=10)
+x = torch.linspace(0, 1, 100).reshape(-1, 1)
+y = model(x).detach().numpy()
+plt.plot(x, y, color = "red")
 plt.show()
+
+
+dataset = torchvision.datasets.MNIST(
+    root = ".tmp",
+    train = True,
+    transform = None,
+    download = True,
+)
+
+x, label = dataset[0]
+print(f"size = {len(dataset)}")
+print(f"type = {type(x)}")
+print(f"label = {label}")
+
+plt.imshow(x, cmap = "gray")
+plt.show()
+
+
+transform = transforms.ToTensor()
+
+dataset = torchvision.datasets.MNIST(
+    root = ".tmp",
+    train = True,
+    transform = transform,
+    download = True,
+)
+
+x, label = dataset[0]
+print(f"size = {len(dataset)}")
+print(f"type = {type(x)}")
+print(f"label = {label}")
+
+
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size = 32,
+    shuffle = True,
+)
+
+for x, label in dataloader:
+    print(f"x.shape = {x.shape}")
+    print(f"label.shape = {label.shape}")
+    break
 
